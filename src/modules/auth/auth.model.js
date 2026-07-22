fe0199abd9277a71dfbfdf37dafccbf9349d0f0d
Gagan pandey <millionairegagan123@gmail.com>
2026-07-22 22:49:32 +0530

    added  schemamodel foraurth related

--- src/modules/auth/auth.model.js
+++ src/modules/auth/auth.model.js
@@ -1,11 +1,72 @@
 import mongoose from 'mongoose';
 
-const authSchema = new mongoose.Schema(
- {
-    fullName:{
-        t
-    }
- }
+const registrationRequestSchema = new mongoose.Schema(
+    {
+name: {
+    type: String,
+    required: [true,"Name is required"],
+    trim: true,
+    minlength: 3,
+    maxlength: 50
+},
+   fatherName: {
+      type: String,
+      required: [true, "Father's name is required"],
+      trim: true,
+      minlength: 3,
+      maxlength: 50,
+    },
 
 
-)
+    mobileNumber: {
+        type:String,
+        required:[true ,"Mobile number is required"],
+        trim : true ,
+        unique: true,
+        index : true ,
+        match:[/^[6-9]\d{9}$/,"Please  enter a valid mobile number "],
+
+},   section: {
+      type: String,
+      required: [true, "Section is required"],
+      trim: true,
+    },
+       studentClass: {
+      type: String,
+      required: [true, "Class is required"],
+      trim: true,
+    },
+  otp: {
+      type: String,
+      select: false,
+    },
+
+    otpExpiresAt: {
+      type: Date,
+    },
+     otpVerified: {
+      type: Boolean,
+      default: false,
+    },
+  status: {
+      type: String,
+      enum: ["PENDING", "APPROVED", "REJECTED"],
+      default: "PENDING",
+      index: true,
+    },
+},
+  {
+    timestamps: true,
+    versionKey: false,
+  
+
+    });
+
+    // above we have created schema and we have created model now 
+    const RegistrationRequest = mongoose.model(
+  "RegistrationRequest",
+  registrationRequestSchema
+  // this is model 
+);
+
+export default RegistrationRequest;// herewe are exporting model 
